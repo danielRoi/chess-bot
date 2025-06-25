5094a4bb33fcb41ef1e0f4a1d48ca020ed5e792d
danielRoi <dushdush123@gmail.com>
2025-06-25 15:42:43 +0300

    replaced list with Span in GetAllAvaailableMoves

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -29,7 +29,7 @@ namespace ChessApp
             {
                 //ChessLogic.SetPositionFromFEN("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10 \r\n");
                 //RefreshBoardFromLogic();
-                //testPrefit();
+                testPrefit();
                 //ChessLogic.PerftWithLogging(5, true, "perfit/prefit_log1.txt"); // Run Perft with logging for depth 5
             }
 
